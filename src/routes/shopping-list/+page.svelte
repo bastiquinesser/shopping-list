c0ddd822483c730927634433a57f9dd464198c35
0302BaStI <quinesser.sebastian@gmail.com>
2024-01-23 11:50:43 +0100

    Update +page.svelte

---
+++ src/routes/shopping-list/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	let list = [];
+	let item = 'xyz';
+</script>
+
+<input type="text" bind:value={item} />
+
+<button on:click={() => list.push(item)}>AddItem</button>
+<div>
+	{item}
+</div>
